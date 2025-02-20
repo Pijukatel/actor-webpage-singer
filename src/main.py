0927fbe1c318c1f7da656e0d5f6fdb997612ebae
Josef Prochazka <josef.prochazka@apify.com>
2025-02-20 14:41:53 +0100

    Actor charge

--- src/main.py
+++ src/main.py
@@ -22,18 +22,15 @@ async def main() -> None:
         lyrics = await generate_lyrics(content)
         Actor.log.info("Generated lyrics", extra={"lyrics": lyrics })
 
-        # Actor charge based on which key is used
-        user_api_key = actor_input.get('topmediai_api_key')
-        actor_api_key = os.environ.get("TOPMEDIAI_API_KEY")
-        if user_api_key:
-            topmediai_api_key = user_api_key
-
-            #await Actor.charge(event_name='song_with_user_api_key',count=1)
-        else:
-            topmediai_api_key = actor_api_key
-            #await Actor.charge(event_name='song_with_actor_api_key', count=1)
-
         # Generate song.
+        topmediai_api_key = actor_input.get('topmediai_api_key') or os.environ.get("TOPMEDIAI_API_KEY")
+
         song_link = generate_song(lyrics=lyrics, api_key=topmediai_api_key, genre=actor_input.get('song_genre'), logger=Actor.log)
         kvs = await Actor.open_key_value_store()
         await kvs.set_value(key="song", value=get_song(song_link), content_type="audio/mpeg")
+
+        # Charge user based on the resources used
+        if actor_input.get('topmediai_api_key'):
+            await Actor.charge(event_name='song_with_user_api_key', count=1)
+        else:
+            await Actor.charge(event_name='song_with_actor_api_key', count=1)
