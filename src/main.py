12704e2889941076a4d289a1c89893e56f5a7800
Josef Prochazka <josef.prochazka@apify.com>
2025-02-20 12:35:50 +0100

    Use shortened lyrics

--- src/main.py
+++ src/main.py
@@ -61,6 +61,6 @@ Tam přemluvil kamarádku ke společné dovolené na Novém Zélandu a druhý de
         # Generate song.
         api_key = actor_input.get('topmediai_api_key')
         Actor.log.info(f"Generating song using: {api_key}")
-        song_link = generate_song(lyrics=content, api_key=api_key, genre="Hip Hop", logger=Actor.log)
+        song_link = generate_song(lyrics=lyrics, api_key=api_key, genre="Hip Hop", logger=Actor.log)
         kvs = await Actor.open_key_value_store()
         kvs.set_value(key="generated_song", value=song_link)
